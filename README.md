e5f1a2c1bec13efb42dc944b69ee53f283083040
Cesar <andrespolindara@gmail.com>
2025-06-12 09:55:42 +0200

    Primera prueba.

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# pruebaVacante01
+# Prueba de razonamiento geométrico y manejo de Python
+
+Esta prueba tiene como propósito medir las aptitudes en razonamiento geométrico y manejo de Python de los candidatos a la vacante "Desarrollo de aplicación CAD".
+La prueba tiene dos partes a saber: Primero, se presenta una estructura de datos, definida por una clase en Python, para representar una geometría 2-dimensional. Segundo, se plantea un problema a resolver.
+
+Para trabajar en esta prueba se deberá contar con un intérprete de Python y los siguientes módulos instalados:
+* Numpy
+* Matplotlib
+
+## Representación de una geometría 2-dimensional en Python
+
+Se tiene una geometría 2-dimensional, también llamada ``malla``, hecha de puntos y de líneas. Se define la clase ``Mesh`` para representar dicha geometría con los siguientes atributos:
+
+* ``vertices``: Es un arreglo bidimensional (Numpy.ndarray) de tamaño $n_v$ x 2, donde $n_v$ es el número de vertices o puntos.
+* ``elements``: Es una lista anidada de tamaño $n_{el}$, donde $n_{el}$ es el número de líneas ó elementos, así cada objeto en esta lista será un elemento definido por los indices de dos vértices.
+* ``elsets``: Es un diccionario que contiene subsets de los elementos. La llave es una cadena de texto y el valor será una lista con los índices de los elementos. A cada una de estas listas se le llama ``elset``.
+
+
+## Problema
+
+### Introducción
+
+Se define una malla ``msh``para representar un círculo con tres ``elset`` a saber:
+* ``msh.elsets['all']``: Lista continua de elementos desde 1 hasta $n_{el}$.
+* ``msh.elsets['upper']``: Lista con los índices de los elementos que conforman la mitad superior del círculo.
+* ``msh.elsets['bottom']``: Lista con los índices de los elementos que conforman la mitad inferior del círculo.
+
+La malla esta ordenada, es decir, la secuencia de los elementos en ``msh.elements`` forma un lazo cerrado en tanto que el segundo vértice de un elemento $i$ coincide siempre con el primer vértice de un elemento $i+1$.
+
+![Malla ordenada 1: Todos los elementos.](./doc/sorted01.svg)
+
+*Figura 1. Malla ordenada: Se presentan los elementos msh.elsets['all'] así como su conectividad.*
+
+![Malla ordenada 2: Elementos superiores.](./doc/sorted02.svg)
+
+*Figura 2. Malla ordenada: Se presentan los elementos msh.elsets['upper'].*
+
+![Malla ordenada 3: Elementos inferiores.](./doc/sorted03.svg)
+
+*Figura 3. Malla ordenada: Se presentan los elementos msh.elsets['bottom'].*
+
+Se define así mismo una función ``shuffle`` en la clase ``Mesh``. Esta función sirve para desordenar los elementos de una malla cambiando el orden de la lista ``elements`` así como el sentido de algunos elementos de forma aleatoria. Es importante notar las siguientes dos cosas que no cambian al invocar la función ``shuffle`` en la malla ``msh``.
+
+1. El listado de elementos en ``msh.elsets['all']``, pues será siempre una lista continua desde 1 hasta $n_{el}$.
+2. El orden de los vértices.
+
+
+![Malla desordenada 1: Todos los elementos.](./doc/shuffled01.svg)
+
+*Figura 1. Malla desordenada: Se presentan los elementos msh.elsets['all'] así como su conectividad.*
+
+![Malla desordenada 2: Elementos superiores.](./doc/shuffled02.svg)
+
+*Figura 2. Malla desordenada: Se presentan los elementos msh.elsets['upper'].*
+
+![Malla desordenada 3: Elementos inferiores.](./doc/shuffled03.svg)
+
+*Figura 3. Malla desordenada: Se presentan los elementos msh.elsets['bottom'].*
+
+### Planteamiento del problema
+
+El fichero [prueba01.py](./prueba01.py) contiene la implementación de la clase ``Mesh`` así como de la función ``draw`` para dibujar mallas. Se deberá escribir una functión ``sort`` para la clase ``Mesh`` que organice nuevamente los elementos desordenados al invocar la función ``shuffle``.
+
+La función ``sort`` deberá cumplir con los siguientes requisitos:
+
+1. La función deberá ordenar la lista ``msh.elements`` de forma tal que los elementos formen un lazo cerrado. La posición del primer elemento y la orientación del mismo es arbitraria.
+2. La función deberá garantizar el mapeo de cada uno de los ``elset`` en ``msh.elsets``, es decir que se deberá poder dibujar nuevamente los elementos superiores e inferiores de la circunferencia.
+3. No se podrá usar ninguna información calculada dentro de la función ``shuffle``.
---
+++ prueba01.py
@@ -0,0 +1,244 @@
+import numpy as np
+from numpy.linalg import norm
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import copy as copy
+import random
+import pickle
+
+from cqfuns3.utils import sort_closed_loop
+
+class Mesh():
+
+    def __init__(self, vertices, elements, nsets={}, elsets={}):
+        self.__vertices = np.array(vertices)
+        self.__elements = copy.deepcopy(elements)
+        self.__nsets = copy.deepcopy(nsets)
+        self.__elsets = copy.deepcopy(elsets)
+        self.__nelements = len(self.__elements)
+        self.__nvertices = len(self.__vertices)
+
+    @property
+    def vertices(self):
+        return self.__vertices
+
+    @property
+    def elements(self):
+        return self.__elements
+
+    @property
+    def nvertices(self):
+        return  self.__nvertices
+
+    @property
+    def nelements(self):
+        return self.__nelements
+
+    @property
+    def x_min(self):
+        return np.min(self.__vertices[:,0])
+
+    @property
+    def x_max(self):
+        return np.max(self.__vertices[:,0])
+
+    @property
+    def y_min(self):
+        return np.min(self.__vertices[:,1])
+
+    @property
+    def y_max(self):
+        return np.max(self.__vertices[:,1])
+
+    @property
+    def center(self):
+        x = 0.5*(self.x_min + self.x_max)
+        y = 0.5*(self.y_min + self.y_max)
+        return np.array([x,y])
+
+    @property
+    def elsets(self):
+        return self.__elsets
+
+    @property
+    def nsets(self):
+        return self.__nsets
+
+    def shuffle(self):
+        # Get indices and shuffle elements
+        indices = [ii for ii in range(self.__nelements)]
+        random.shuffle(indices)
+
+        elements = [self.__elements[ii] for ii in indices]
+        self.__elements = elements
+        for key,value in self.__elsets.items():
+            self.__elsets[key] = [indices.index(ii) for ii in value]
+
+        self.__elsets['all'] = [ii for ii in range(self.__nelements)]   # special hardcoded set!!!
+
+        # Get indices for half the elements and flip elements
+        indices = random.sample(range(len(self.__elements)), k=self.__nelements // 2)
+        for ii in indices:
+            self.__elements[ii].reverse()
+
+    def sort(self):
+        # ###################################################################################### #
+        # solution
+
+        # ###################################################################################### #
+        
+        self.__elsets['all'] = [ii for ii in range(self.__nelements)]   # special hardcoded set!!!
+
+    def save(self, filename='pickle.pkl'):
+        save_object_to_pickle((self.__vertices, self.__elements, self.__elsets), filename)
+
+
+
+def set_axes_equal(ax, my_mesh:Mesh):
+
+    x_range = my_mesh.x_max - my_mesh.x_min
+    y_range = my_mesh.y_max - my_mesh.y_min
+    r = max(x_range, y_range)
+
+    x_left = my_mesh.center[0] - r
+    x_right = my_mesh.center[0] + r
+    y_bottom = my_mesh.center[1] - r
+    y_upper = my_mesh.center[1] + r
+    ax.set_xlim([x_left, x_right])
+    ax.set_ylim([y_bottom, y_upper])
+    return (x_left, x_right, y_bottom, y_upper)
+
+
+def draw(my_mesh:Mesh, elset=[], colormap ='cividis', title='', annotate=False):
+
+    color_elements = False
+    cmap = cm.get_cmap(colormap, my_mesh.nelements)  # Get cyclic color map
+    norm = colors.Normalize(vmin=0, vmax=my_mesh.nelements - 1)  # Normalize to the range of data
+    if elset != []:
+        # cmap = cm.get_cmap(colormap, len(elset))  # Get cyclic color map
+        # norm = colors.Normalize(vmin=0, vmax=len(elset) - 1)  # Normalize to the range of data
+        color_elements = True
+
+    # draw 2D Mesh
+    fig, ax = plt.subplots()
+
+    for indx, el in enumerate(my_mesh.elements):
+        nel = len(el)
+        offset = 1
+        if nel == 2:
+            offset = 0
+
+        x = []
+        y = []
+        for jj in range(nel-1+offset):
+            el_1 = el[jj%nel]
+            v_1 = np.array(my_mesh.vertices[el_1])
+            x.append(v_1[0])
+            y.append(v_1[1])
+            el_2 = el[(jj+1)%nel]
+            v_2 = np.array(my_mesh.vertices[el_2])
+            x.append(v_2[0])
+            y.append(v_2[1])
+
+            dv = v_2 - v_1
+
+            ax.plot(x, y, '-k', linewidth=1, zorder=1)
+
+            if color_elements and indx in elset:
+                # aux_indx = elset.index(indx)
+                # color = cmap(norm(aux_indx))  # Get color from color map
+                color = cmap(norm(indx))  # Get color from color map
+                # ax.plot(x, y, color=color, linewidth=4.0)
+                ax.quiver(v_1[0], v_1[1], dv[0], dv[1], zorder=2,
+                          angles='xy', scale_units='xy', scale=1, width=0.0075, color=color, label='%d: (%d, %d)'%(indx,el_1,el_2))
+
+    x_left, x_right, y_bottom, y_upper = set_axes_equal(ax, my_mesh)
+    ax.set_xlim()
+    ax.set_aspect('equal','box')
+    # ax.set_axis_off()
+    ax.set_title(title)
+
+    if annotate:
+        for ii,v in enumerate(my_mesh.vertices):
+            ax.plot(v[0], v[1], 'ok', markersize=3)
+            ax.annotate('%d'%ii, xy=(v[0], v[1]))#,
+
+        ax.legend(loc='upper left', bbox_to_anchor=(1., 1.))
+        # ax.legend()
+
+    plt.show()
+    return fig, ax
+
+
+def save_object_to_pickle(obj, file_path):
+    with open(file_path, 'wb') as f:
+        pickle.dump(obj, f)
+
+def load_from_pickle(file_path):
+    with open(file_path, 'rb') as f:
+        data = pickle.load(f)
+    return data
+
+
+def create_circle(center=(0., 0.), radius=1.0, nelements=24):
+
+    # create vertices
+    vertices = []
+    center = np.array(center)
+    for a in np.linspace(0., 2*np.pi, nelements+1)[:-1]:
+        p = center + radius*np.array((np.cos(a), np.sin(a)))
+        vertices.append(p)
+
+    # create elements
+    elements = [[ii,(ii+1)%nelements] for ii in range(nelements)]
+
+    # create sets
+    nsets = {'all': [ii for ii in range(nelements)]}
+    elsets = {
+        'all': [ii for ii in range(nelements)],
+        'upper': [ii for ii in range(nelements//2)],
+        'bottom': [ii for ii in range(nelements // 2, nelements)],
+    }
+
+    return Mesh(vertices, elements, nsets, elsets)
+
+
+# #################################################################################################################### #
+
+msh = create_circle(nelements=12)
+elset_all = msh.elsets['all']
+elset_upper = msh.elsets['upper']
+elset_bottom = msh.elsets['bottom']
+
+fig,_ = draw(msh, elset=elset_all, annotate=True)
+fig.savefig('sorted01.svg')
+
+fig,_ = draw(msh, elset=elset_upper)
+fig.savefig('sorted02.svg')
+
+fig,_ = draw(msh, elset=elset_bottom)
+fig.savefig('sorted03.svg')
+
+msh.shuffle()
+elset_all = msh.elsets['all']
+elset_upper = msh.elsets['upper']
+elset_bottom = msh.elsets['bottom']
+
+fig,_ = draw(msh, elset=elset_all, annotate=True)
+fig.savefig('shuffled01.svg')
+
+fig,_ = draw(msh, elset=elset_upper)
+fig.savefig('shuffled02.svg')
+
+fig,_ = draw(msh, elset=elset_bottom)
+fig.savefig('shuffled03.svg')
+
+
+# msh.sort()
+# elset_all = msh.elsets['all']
+# elset_upper = msh.elsets['upper']
+# elset_bottom = msh.elsets['bottom']
+# draw(msh, elset=elset_all, annotate=True)
+# draw(msh, elset=elset_upper)
+# draw(msh, elset=elset_bottom)
